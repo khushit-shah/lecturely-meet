7baebc9b1cfae53b0f7c4bdd21daae63b5e6c2f7
Khushit Shah <khushitshah1@gmail.com>
2021-03-13 18:12:05 +0530

    Added Circle Full Functionality!

--- lecturely-meet-extension/js/client.js
+++ lecturely-meet-extension/js/client.js
@@ -1,3 +1,12 @@
+// utility functions:
+function addCircle(div, x_pos, y_pos) {
+    // div.style.position = "absolute";
+    div.style.left = x_pos + 'px';
+    div.style.top = y_pos + 'px';
+    // div.style.zIndex = "1";
+    div.classList.add("circle-added");
+}
+
 window.onload = start;
 
 function start() {
@@ -24,6 +33,7 @@ function start() {
         bubbles: true,
         cancelable: true
     }));
+
     setTimeout(() => {
         let userName = document.querySelector("span.ZjFb7c").innerHTML;
         let userImageLink = document.getElementsByClassName("RWK2Je")[0].getElementsByTagName("img")[0].src;
@@ -36,7 +46,13 @@ function start() {
         console.log(basicStructure);
 
         const socket = io("http://localhost:3000/");
-        
+
+        // create all the elements already!
+        let circle = document.createElement("div");
+        circle.className = "circle";
+        document.body.appendChild(circle);
+
+
         socket.on("connect", () => {
             console.log("Client is connected to server! with id", socket.id);
             socket.emit("init", basicStructure);
@@ -44,7 +60,16 @@ function start() {
 
         socket.on("circle", (data) => {
             console.log("Client got a circle", data);
-
+            let x = data["x"];
+            let y = data["y"];
+            let t = data["t"];
+            let startTime = Date.now();
+            addCircle(circle, x, y);
+            circle.onclick = function () {
+                circle.className = "circle";
+                let endTime = Date.now();
+                sendCircleResponse((endTime - startTime) / 1000);
+            }
         });
 
         socket.on("info", (data) => {
@@ -64,7 +89,7 @@ function start() {
         })
 
         function sendCircleResponse(resTime) {
-            console.log("sending circle response");
+            console.log("sending circle response ", resTime);
             socket.emit("circleRes", {
                 ...basicStructure,
                 "resTime": resTime,
